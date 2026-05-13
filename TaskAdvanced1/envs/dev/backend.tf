7fbd23b7073cfdc3d60c66e3a599f6b4c08b3def
Baratashvili Vadim <bar_vad@list.ru>
2026-05-13 19:36:46 +0300

    test

--- TaskAdvanced1/envs/dev/backend.tf
+++ TaskAdvanced1/envs/dev/backend.tf
@@ -7,8 +7,7 @@ terraform {
     bucket = "practicum-terraform-state-bucket"
     region = "ru-central1"
     key    = "dev/terraform.tfstate"
-
-
+ 
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_requesting_account_id  = true
